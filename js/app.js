6f76fd4337ab44c84b79571294766d0f7aa2fe5b
liushipengmelo <liushiyueyueniao@hotmail.com>
2015-12-25 16:49:12 +0800

    1.搞定对话框的实质

--- js/app.js
+++ js/app.js
@@ -1,67 +1,81 @@
 (function(angular) {
 
-	var app = angular.module('modalDemoApp', ['modalDialog']);
+    var app = angular.module('modalDemoApp', ['modalDialog']);
 
-	app.controller('MainCtrl', ['$scope', '$log', 'modalService', function($scope, $log, modalService) {
-		$scope.buttonLabel = 'Show Modal';
-
-		$scope.modalConfig = {
-			data: {
-				title: 'Title Goes Here',
-				message: 'Message goes here'
-			},
-			include: 'templates/modal-content-template.html',
-			animate: true,
-			buttons: [{
-				type: 'info',
-				label: 'Info',
-				click: function(event, data, instance) {
-					$log.info('Info:', event, data, instance);
-				}
-			},{
-				type: 'success',
-				label: 'Success'
-			},{
-				type: 'warning',
-				label: 'Warning'
-			},{
-				type: 'danger',
-				label: 'Danger'
-			},{
-				label: 'Cancel',
-				click: function(event, data, instance) {
-					instance.dismiss('cancel');
-				}
-			},{
-				type: 'primary',
-				label: 'Update',
-				click: function(event, data, instance) {
-					if (data.title && data.message) {
-						$scope.modalConfig.data.title = data.title;
-						$scope.buttonLabel = data.message;
-						instance.close(data);
-					}
-				}
-			}],
-			keyup: function(event, data, instance) {
-				if (event.which === 13) {
-					if (data.title && data.message) {
-						$scope.modalConfig.data.title = data.title;
-						$scope.buttonLabel = data.message;
-						instance.close(data);
-					}
-				}
-			},
-			select: function(event, data, instance) {
-				$log.info('Select:', event, data, instance);
-				event.target.select();
-			}
-		};
-
-		$scope.showModal = function() {
-			$log.info('Show Modal');
-			modalService.show($scope.modalConfig);
-		};
-	}]);
+    app.controller('mainController', ['$scope', '$log', 'modalService', function($scope, $log, modalService) {
+    	// 初始化按钮的值
+        $scope.buttonLabel = 'Show Modal';
+        // 要弹出的对话框的数据和回调的函数
+        $scope.modalConfig = {
+            data: {
+                title: 'begin title',
+                message: 'bagin Message'
+            },
+            include: 'templates/modal-content-template.html',
+            animate: true,
+            buttons: [{
+                type: 'info',
+                label: 'Info',
+                click: function(event, data, instance) {
+                    $log.info('Info:', event, data, instance);
+                }
+            }, {
+                type: 'success',
+                label: 'Success',
+                click: function(event, data, instance) {
+                    $log.info('success:', event, data, instance);
+                }
+            }, {
+                type: 'warning',
+                label: 'Warning',
+                click: function(event, data, instance) {
+                    $log.info('warning:', event, data, instance);
+                }
+            }, {
+                type: 'danger',
+                label: 'Danger',
+                click: function(event, data, instance) {
+                    $log.info('danger:', event, data, instance);
+                }
+            }, {
+                label: 'Cancel',
+                click: function(event, data, instance) {
+                    $log.info('csancel:', event, data, instance);
+                    instance.dismiss('cancel');
+                }
+            }, {
+                type: 'primary',
+                label: 'Update',
+                click: function(event, data, instance) {
+                    if (data.title && data.message) {
+                        $log.info('Update:', event, data, instance);
+                        $scope.modalConfig.data.title = data.title;
+                        $scope.buttonLabel = data.message;
+                        instance.close(data);
+                    }
+                }
+            }],
+            keyup: function(event, data, instance) {
+                $log.info('keyup:', event, data, instance);
+                if (event.which === 13) {
+                    if (data.title && data.message) {
+                        $scope.modalConfig.data.title = data.title;
+                        $scope.buttonLabel = data.message;
+                        instance.close(data);
+                    }
+                }
+            },
+            select: function(event, data, instance) {
+                $log.info('Select:', event, data, instance);
+                event.target.select();
+            }
+        };
+        // 界面按钮
+        $scope.showModal = function() {
+            $log.info('Show Modal');
+            // 调用对话框服务，出入上面定义的消息
+            modalService.show($scope.modalConfig);
+        };
+    }]);
 
 })(window.angular);
--- js/modal-dialog.js
+++ js/modal-dialog.js
@@ -1,38 +1,53 @@
 (function(angular) {
 
-	var module = angular.module('modalDialog', ['ngAnimate', 'ui.bootstrap']);
+    var module = angular.module('modalDialog', ['ngAnimate', 'ui.bootstrap']);
 
-	module.controller('ModalCtrl', ['$scope', '$modalInstance', 'config', function($scope, $modalInstance, config) {
-		$scope.config = config;
-		$scope.data = angular.copy(config.data);
-		$scope.instance = $modalInstance;
-	}]);
+	    module.controller('ModalCtrl', ['$scope', '$modalInstance', 'config', function($scope, $modalInstance, config) {
+	        // 初始化control的数据，使用依赖注入的值
+	        $scope.config = config;
+	        $scope.data = angular.copy(config.data);
+	        $scope.instance = $modalInstance;
+	    }]);
 
-	module.factory('modalService', ['$log', '$modal', function($log, $modal) {
-		return {
-			show: function(config) {
-				var instance = $modal.open({
-					animation: config.animate === true,
-					templateUrl: 'templates/modal-template.html',
-					controller: 'ModalCtrl',
-					resolve: {
-						config: function() {
-							return config;
-						}
-					}
-				});
+	    module.factory('modalService', 
+	    	['$log', '$modal', function($log, $modal) {
+		        return {
+		            // 调用服务室传入的对象，返回的还是config对象
+		            show: function(config) {
+		            	// config的作用域
+		                var instance = $modal.open({
+		                    animation: config.animate === true,
+		                    // 在这个模板中使用config对象的属性
+		                    templateUrl: 'templates/modal-template.html',
+		                    controller: 'ModalCtrl',
+		                    resolve: {
+		                        config: function() {
+		                            return config;
+		                        }
+		                    }
+		                });
+		                // instance是正在打开的对象
+		                instance.opened.then(function(result) {
+		                    // 可以调用代码做一些事情
+		                    $log.info('Opened:', result);
 
-				instance.opened.then(function(result) {
-					$log.info('Opened:', result);
-				});
+		                });
+		                // 
+		                instance.result.then(
+		                	// 第一个参数是关闭时的回调
+		                	function(result) {
 
-				instance.result.then(function(result) {
-					$log.info('Closed:', result);
-				}, function(reason) {
-					$log.info('Dismissed:', reason);
-				});
-			}
-		};
-	}]);
+		                        $log.info('Closed:', result);
+
+		                    },
+		                    // 第二个参数是消失时的回调
+		                    function(reason) {
+		                        $log.info('Dismissed:', reason);
+		                    }
+		                );
+		            }
+		        };
+	   		}]
+	    );
 
 })(window.angular);
